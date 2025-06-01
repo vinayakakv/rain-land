ced0023aa764f35bc5e5261aa59660d38d39f5a3
Vinayaka K V <me.vinayakakv@gmail.com>
2025-06-01 10:38:01 +0530

    Replace message.from with message.author since the former always returned the group ID

---
+++ data-collector/src/prepareMessages.ts
@@ -0,0 +1,31 @@
+type BaseMessage = {
+  timestamp: number
+  senderId: string
+  text: string
+}
+
+const combineText = <T extends BaseMessage>(messages: T[]) => {
+  const first = messages.at(0)
+  if (!first) return null
+  return { ...first, text: messages.map((message) => message.text).join('\n') }
+}
+
+const aggregateSenderMessages = <T extends BaseMessage>(messages: T[]) => {
+  const senderGroups = Object.groupBy(messages, (message) => message.senderId)
+  return Object.values(senderGroups)
+    .map((messagesBySender) => combineText(messagesBySender || []))
+    .filter(Boolean)
+}
+
+export const prepareMessages = <T extends BaseMessage>(messages: T[]) => {
+  const messagesWithDate = messages.map((message) => ({
+    ...message,
+    date: new Date(message.timestamp * 1000),
+  }))
+  const dayAggregations = Object.groupBy(messagesWithDate, (messages) =>
+    messages.date.toDateString(),
+  )
+  return Object.values(dayAggregations).flatMap((messagesOnDay) =>
+    aggregateSenderMessages(messagesOnDay || []),
+  )
+}
